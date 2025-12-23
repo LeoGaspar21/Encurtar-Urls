52f03275a0421a8ae5cadb793f7b5bf0bf88422b
leonardo <leonardogaspar2001@gmail.com>
2025-12-23 15:24:43 -0300

    Primeiro commit

---
+++ src/main/java/com/leogaspar/EncurtarURL/EncurtarUrLsApplication.java
@@ -0,0 +1,13 @@
+package com.leogaspar.EncurtarURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtarUrLsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtarUrLsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leogaspar/EncurtarURL/EncurtarUrLsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leogaspar.EncurtarURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtarUrLsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
