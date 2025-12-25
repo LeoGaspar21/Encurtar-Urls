9f2d8be278c407f56bd8ef6a16340beb10dd1759
leonardo <leonardogaspar2001@gmail.com>
2025-12-25 17:58:12 -0300

    Implementacao de DTOs para comunicação da API

---
+++ src/main/java/com/leogaspar/EncurtarURL/dto/UrlResponseDTO.java
@@ -0,0 +1,23 @@
+package com.leogaspar.EncurtarURL.dto;
+
+public class UrlResponseDTO {
+	
+	private String shortUrl;
+	
+	
+	public UrlResponseDTO() {
+		
+	}
+
+
+	public String getShortUrl() {
+		return shortUrl;
+	}
+
+
+	public void setShortUrl(String shortUrl) {
+		this.shortUrl = shortUrl;
+	}
+	
+	
+}
